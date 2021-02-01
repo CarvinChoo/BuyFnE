2610a9bb05ca1196fc0ddf377799cf31575973de
Carvin <carvin.choo@gmail.com>
2021-02-01 20:04:32 +0800

    flexBasis, flexGrow, flexShrink

--- App.js
+++ App.js
@@ -7,30 +7,30 @@ export default function App() {
     <View
       style={{
         backgroundColor: "#fff",
-        flex: 1, // the container
-        flexDirection: "row", //set primary axis, using row makes it align from left to right
-        // default direction is top to bottom from the left
-        justifyContent: "center", //align items along main/primary axis
-        alignItems: "center", //align items along secondary axis within each line
-        //baseline enforces same bottom line for all
-        //stretch forces baseline to be on the top
-        alignContent: "center", // determines the alignment of the entire content as a whole, only has effect is used with flexWrap
-        flexWrap: "wrap", // forces items that are too big to stay in 1 line to move to another line while keeping their intended size
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
       }}
     >
       <View
         style={{
           backgroundColor: "dodgerblue",
-          // flex: 2,
-          width: 100,
-          height: 300,
-          // alignSelf: "flex-start", // make this particular element align with "start" for primary axis or "end" for end of primary axis as baseline
+          // flexBasis: 100, // if element primay axis is vertical, this can be used to set width , if horizontal, it is used to set height
+          // virtually identical to width or height
+          //flexGrow: 1, // same as setting flex property but does not require refresh
+          // same as
+          // flex: 1,
+          width: 400,
+          // flexShrink: 1, // if there is overflow, this item will resize so other items can fit
+          // same as
+          flex: -1,
+          height: 100,
         }}
       />
       <View
         style={{
           backgroundColor: "gold",
-          // flex: 1,
           width: 100,
           height: 100,
         }}
@@ -38,26 +38,6 @@ export default function App() {
       <View
         style={{
           backgroundColor: "tomato",
-          // flex: 1, // all flex 1 in a container will result in even divde
-          // other flex will result in them filling bigger ratios
-          width: 100,
-          height: 100,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "grey",
-          // flex: 1, // all flex 1 in a container will result in even divde
-          // other flex will result in them filling bigger ratios
-          width: 100,
-          height: 100,
-        }}
-      />
-      <View //overflow causes some items to get shrank but doesnt change others
-        style={{
-          backgroundColor: "greenyellow",
-          // flex: 1, // all flex 1 in a container will result in even divde
-          // other flex will result in them filling bigger ratios
           width: 100,
           height: 100,
         }}
