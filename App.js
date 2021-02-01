636f4ed05d80704e850ff67f5fdc9a7cfdf535f0
Carvin <carvin.choo@gmail.com>
2021-02-01 23:15:00 +0800

    requires importing from "@expo/vector-icons" and looking up https://icons.expo.fyi/

--- App.js
+++ App.js
@@ -2,6 +2,9 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
+// Import from "@expo/vector-icons" to use icons
+import { MaterialCommunityIcons } from "@expo/vector-icons"; // check using https://icons.expo.fyi/ for available icon families
+
 export default function App() {
   return (
     <View
@@ -11,36 +14,11 @@ export default function App() {
         alignItems: "center",
       }}
     >
-      <View
-        style={{
-          backgroundColor: "dodgerblue",
-          width: 100,
-          height: 100,
-          //Padding
-          padding: 20,
-          paddingHorizontal: 10, // overwrites general padding option
-          paddingLeft: 30, //Overwrites horizontal padding
-        }}
-      >
-        <View
-          style={{
-            backgroundColor: "gold",
-            width: 50,
-            height: 50,
-          }}
-        ></View>
-      </View>
-      <View
-        style={{
-          backgroundColor: "tomato",
-          width: 100,
-          height: 100,
-          //Margin
-          margin: 20,
-          marginVertical: 30, //Overwrites if conflicting
-          marginRight: 50, // Overwrites if conflicting
-        }}
-      ></View>
+      <MaterialCommunityIcons // Icon Component
+        name='email' // check using https://icons.expo.fyi/ for available icon names
+        size={200}
+        color='dodgerblue' // uses same color code as react native
+      />
     </View>
   );
 }
