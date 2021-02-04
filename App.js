7ba96e71cbd6ae80fece39ffd064ba26101770f6
Carvin <carvin.choo@gmail.com>
2021-02-04 16:11:07 +0800

    done

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
@@ -19,7 +19,27 @@ export default function App() {
   // return <ViewImageScreen />;
   // return <MessagesScreen />;
   // return <AccountScreen />;
-  return <ListingsScreen />;
+  // return <ListingsScreen />;
+  const [firstName, setFirstName] = useState("");
+  return (
+    <Screen>
+      {/* updated as user types something */}
+      <Text>{firstName}</Text>
+      <TextInput
+        // secureTextEntry={true} //hides text like a password
+        secureTextEntry //can also just use properties name to auto set to true
+        clearButtonMode='always' //only works on iOS, shows a clear all button in text box
+        keyboardType='numeric' // sets keyboard used, use alt-space to see types
+        maxLength={5} // max length user can type
+        onChangeText={(text) => setFirstName(text)} // will activate as user types something into text box, for now it sets the state for firstName
+        placeholder='First Name' // default background shown text if nothing is typed.
+        style={{
+          borderBottomColor: "#ccc", //a bottom border color
+          borderBottomWidth: 1, //a bottom border width
+        }}
+      />
+    </Screen>
+  );
 }
 
 const styles = StyleSheet.create({
