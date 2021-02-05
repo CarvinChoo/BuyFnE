f5c291919da8aea3b6d6a266469a6fcd470bb5f8
Carvin <carvin.choo@gmail.com>
2021-02-06 02:03:29 +0800

    Scroll Image List sideways, Upload Icon always on Screen

--- app/components/ImageInputList.js
+++ app/components/ImageInputList.js
@@ -1,28 +1,39 @@
-import React from "react";
-import { StyleSheet, View } from "react-native";
+import React, { useRef } from "react";
+import { ScrollView, StyleSheet, View } from "react-native";
 import ImageInput from "./ImageInput";
 
 function ImageInputList(
   { imageUris = [], onRemoveImage, onAddImage } //default empty array of images
 ) {
-  return (
-    <View style={styles.container}>
-      {/* split array into individual images, view is used to give each image right margin */}
-      {imageUris.map((uri) => (
-        <View key={uri} style={styles.image}>
-          <ImageInput // each image is than inserted into an ImageInput to be displayed
-            imageUri={uri}
-            key={uri} // unique identifier
-            onChangeImage={(null_sent_from_ImageInput) => onRemoveImage(uri)} // takes uri from current ImageInput component and request to remove it from parent
-          />
-        </View>
-      ))}
+  // used to call a function on a Component///
+  const scrollView = useRef(); // refernce to an instance of a component
+  //////////////////////////////////////////
 
-      {/* Empty ImageInput waiting for image to be added */}
-      <ImageInput
-        onChangeImage={(uri) => onAddImage(uri)} // only adding action can happen for an empty image
-      />
-    </View>
+  return (
+    <ScrollView
+      ref={scrollView} // to tell scrollView that this is the instance component we are referencing
+      horizontal={true}
+      onContentSizeChange={() => scrollView.current.scrollToEnd()} //When component changes size, execute an event,
+      //scrollToEnd() is a method found in ScrollView documentation
+    >
+      <View style={styles.container}>
+        {/* split array into individual images, view is used to give each image right margin */}
+        {imageUris.map((uri) => (
+          <View key={uri} style={styles.image}>
+            <ImageInput // each image is than inserted into an ImageInput to be displayed
+              imageUri={uri}
+              key={uri} // unique identifier
+              onChangeImage={(null_sent_from_ImageInput) => onRemoveImage(uri)} // takes uri from current ImageInput component and request to remove it from parent
+            />
+          </View>
+        ))}
+
+        {/* Empty ImageInput waiting for image to be added */}
+        <ImageInput
+          onChangeImage={(uri) => onAddImage(uri)} // only adding action can happen for an empty image
+        />
+      </View>
+    </ScrollView>
   );
 }
 
