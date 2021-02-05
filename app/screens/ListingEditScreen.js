52dcdba8504c8f8e10a3f6034d7ec9c9b273c3a2
Carvin <carvin.choo@gmail.com>
2021-02-05 23:13:04 +0800

    Organized Again

--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -17,7 +17,7 @@ const validationSchema = Yup.object().shape({
   title: Yup.string().required().min(1).label("Title"),
   price: Yup.number().required().min(1).max(10000).label("Price"),
   description: Yup.string().label("Description"),
-  category: Yup.object().required().nullable().label("Category"),
+  categories: Yup.object().required().nullable().label("Category"),
 });
 
 const categories = [
