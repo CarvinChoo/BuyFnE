adf38c2e393cb59ec3a30de79516413950dcfe61
Carvin <carvin.choo@gmail.com>
2021-02-06 16:05:57 +0800

    Simple Navigation Screen to Screen

--- App.js
+++ App.js
@@ -9,35 +9,64 @@ import {
   View,
 } from "react-native";
 import * as ImagePicker from "expo-image-picker";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
 
-import ViewImageScreen from "./app/screens/ViewImageScreen";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import AppButton from "./app/components/AppButton";
-import Card from "./app/components/Card";
-import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
-import MessagesScreen from "./app/screens/MessagesScreen";
 import Screen from "./app/components/Screen";
-import Icon from "./app/components/Icon";
-import ListItem from "./app/components/lists/ListItem";
-import AccountScreen from "./app/screens/AccountScreen";
-import ListingsScreen from "./app/screens/ListingsScreen";
-import AppTextInput from "./app/components/AppTextInput";
-import AppPicker from "./app/components/AppPicker";
-import AppText from "./app/components/AppText";
-import LoginScreen from "./app/screens/LoginScreen";
-import ListingEditScreen from "./app/screens/ListingEditScreen";
-import ImageInput from "./app/components/ImageInput";
-import ImageInputList from "./app/components/ImageInputList";
+
+const Link = () => {
+  const navigation = useNavigation(); // using a react-navigation hook to get access to "navigation" property
+  return (
+    <Button title='Click' onPress={() => navigation.navigate("TweetDetails")} />
+  );
+};
+
+const Tweets = (
+  { navigation } // navigation is only availables to components declared as Stack.Screen, child components of Stack.Screen will not get navigation prop
+) => (
+  // storing a page into a function
+  <Screen>
+    <Text>Tweets</Text>
+    <Link />
+    {/* <Button
+      title='View Tweet'
+      onPress={() => navigation.navigate("Tweets")} // navigates to instance of screen, is already there, no duplication
+    /> */}
+
+    {/* <Button
+      title='View Tweet'
+      onPress={() => navigation.push("Tweets")} // duplicates instances of a screen and stack it on top
+    /> */}
+  </Screen>
+);
+
+const TweetDetails = () => (
+  // storing a page into a function
+  <Screen>
+    <Text>Tweets Details</Text>
+  </Screen>
+);
+
+const Stack = createStackNavigator(); //returns  something similar to a component
+const StackNavigator = () => (
+  <Stack.Navigator
+  // initialRouteName='Tweets' can be used to identify initial page
+  >
+    <Stack.Screen // 1st screen within navigation
+      name='Tweets' // header + identifier
+      component={Tweets} // call the function to render page
+    />
+    <Stack.Screen // 2nd screen within navigation
+      name='TweetDetails' // header + identifier
+      component={TweetDetails} // call the function to render page
+    />
+  </Stack.Navigator>
+);
 
 export default function App() {
-  return <ListingEditScreen />;
+  return (
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
+  );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    // backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
