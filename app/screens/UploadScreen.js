bad3dc830ba54f586dd22c5ed4260aec8c2f637b
Carvin <carvin.choo@gmail.com>
2021-02-08 01:03:26 +0800

    Progress Bar + Done animation

--- app/screens/UploadScreen.js
+++ app/screens/UploadScreen.js
@@ -7,20 +7,23 @@ import colors from "../config/colors";
 
 function UploadScreen({ onDone, progress = 0, visible = false }) {
   return (
+    //like a popup
     <Modal visible={visible}>
       <View style={styles.container}>
-        {progress < 1 ? (
-          <Progress.Bar
+        {progress < 1 ? ( // if progress is 100%
+          <Progress.Bar // renders progress bar
             progress={progress}
             color={colors.brightred}
             width={250}
           />
         ) : (
+          // once done, renders done animation
           <LottieView
-            autoplay
-            loop={false}
-            onAnimationFinish={onDone}
+            autoPlay
+            loop={false} //only play once
+            onAnimationFinish={onDone} //raise an event to  tell Listing Edit Screen that the animation is done
             source={require("../assets/animations/done.json")}
+            style={styles.animation}
           />
         )}
       </View>
@@ -30,9 +33,7 @@ function UploadScreen({ onDone, progress = 0, visible = false }) {
 
 const styles = StyleSheet.create({
   container: { alignItems: "center", flex: 1, justifyContent: "center" },
-  animation: {
-    width: 150,
-  },
+  animation: { width: 200 }, // does not change Size
 });
 
 export default UploadScreen;
