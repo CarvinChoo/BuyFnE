762b27813c72535a567989ea38d3d683f624cde9
Carvin <carvin.choo@gmail.com>
2021-02-07 16:22:44 +0800

    Error handling during request to server

--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -7,11 +7,14 @@ import routes from "../navigation/routes";
 import Screen from "../components/Screen";
 import colors from "../config/colors";
 import listingsApi from "../api/listings";
+import AppText from "../components/AppText";
+import AppButton from "../components/AppButton";
 
 function ListingsScreen({ navigation }) {
   // since this is a Stack.Screen, it has access to {navigation} prop
 
-  const [listings, setListings] = useState([]);
+  const [listings, setListings] = useState([]); // use for listing state
+  const [error, setError] = useState(false); // used for error state
 
   useEffect(() => {
     loadListings(); // function to call listings
@@ -20,11 +23,28 @@ function ListingsScreen({ navigation }) {
   // function to call listings
   const loadListings = async () => {
     const response = await listingsApi.getListings(); // awaits for API layer to retreive and gives listings
+    // Error handling
+    if (!response.ok) {
+      // if response returns an error
+      setError(true); //set error state to true
+      return;
+    }
+    setError(false); //means no error
     setListings(response.data); // set current state to listings received
   };
 
   return (
     <Screen style={styles.screen}>
+      {error && ( // if error is detected
+        <>
+          <AppText>Couldn't retrieve the listings.</AppText>
+          <AppButton
+            title='Retry'
+            onPress={loadListings} // button to reload listing request again
+            color='cyan'
+          />
+        </>
+      )}
       <FlatList
         data={listings}
         keyExtractor={(listing) => listing.id.toString()} // unqiue key is alway expected to be a string
